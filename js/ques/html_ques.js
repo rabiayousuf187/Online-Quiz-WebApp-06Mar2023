ab8127cc6ae475ca22b2c86c8553b1d30904e14c
rabiayousuf187 <rabiayousuf187@gmail.com>
2023-04-07 10:27:50 +0500

    move into js/ques folder

---
+++ js/ques/html_ques.js
@@ -0,0 +1,48 @@
+const html_ques = [
+    {
+        "id" : "1",
+        "ques" : "What do you understand by HTML?",
+        "opt" : [
+                "HTML describes the structure of a webpage",
+                "HTML is the standard markup language mainly used to create web pages",
+                "HTML consists of a set of elements that helps the browser how to view the content",
+                "All of the above"
+            ],
+        "ans" : "All of the above",
+    },
+    {
+        "id" : "2",
+        "ques" : "What do you understand by HTML?",
+        "opt" : [
+            "HTML describes the structure of a webpage",
+            "HTML is the standard markup language mainly used to create web pages",
+                "HTML consists of a set of elements that helps the browser how to view the content",
+                "All of the above"
+            ],
+            "ans" : "All of the above",
+        },
+        {
+            "id" : "3",
+            "ques" : "What do you understand by HTML?",
+            "opt" : [
+                "HTML describes the structure of a webpage",
+                "HTML is the standard markup language mainly used to create web pages",
+                "HTML consists of a set of elements that helps the browser how to view the content",
+                "All of the above"
+            ],
+            "ans" : "All of the above",
+        },
+    {
+        "id" : "4",
+        "ques" : "What do you understand by HTML?",
+        "opt" : [
+                "HTML describes the structure of a webpage",
+                "HTML is the standard markup language mainly used to create web pages",
+                "HTML consists of a set of elements that helps the browser how to view the content",
+                "All of the above"
+            ],
+        "ans" : "All of the above",
+    }
+];
+
+export{ html_ques };
