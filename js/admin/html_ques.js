5a5e859a90fc2646cc375da17c7b032f2ab57d13
rabiayousuf187 <rabiayousuf187@gmail.com>
2023-04-07 10:09:57 +0500

    working on HTML CRUD

--- js/admin/html_ques.js
+++ js/admin/html_ques.js
@@ -1,5 +1,6 @@
 let html_ques = [];
-let ques, opt,ans;
+let opt_array = [], opt = [];
+let ques,ans;
 let get_data = localStorage.getItem("html_ques");
 console.log("html ques === push ",get_data)
 if(get_data !== null){
@@ -25,18 +26,39 @@ $(document).ready(function() {
     
 } );
 
+    opt_array = document.getElementById('options').querySelectorAll('[name = opt]');
+    
+ 
+    // function val_fun(item){
+    //     console.log('item.value ==== ',item.value);
+    //     return item.value;
+    // }
+    // opt = opt_array.map(val_fun);
+
 document.getElementById('save_ques_btn').onclick = ()=>{ save_ques()};
 function save_ques(){
-    ques = document.getElementById('html_ques');
-    opt = document.querySelectorAll('opt');
-    ans = document.getElementById('ans');
+    ques = document.getElementById('html_ques').value;
+    ans =  document.getElementById('ans').value;
+    opt_array = document.getElementById('options').getElementsBy('form-control');
+    opt_array = Object.values(opt_array)
+    console.log('opt_array  ==== ',typeof(opt_array));
+    console.log('opt_array  ==== ',opt_array);
+    console.log('opt_array  ==== ',opt_array[0]);
+    console.log('opt_array  ==== ',opt_array[0].value);
+    console.log('opt_array  ==== ',typeof(opt_array[0]));
+    console.log('opt_array  ==== ',typeof(opt_array[0].value));
+ 
+    // function val_fun(item){
+    //     console.log('item.value ==== ',item.value);
+    //     return item.value;
+    // }
+    // opt = opt_array.map(val_fun);
+    // console.log('save_ques ==== ',opt);
 
-    console.log('save_ques ==== ', ques, opt, ans);
+    console.log('save_ques ==== ', ques);
+    console.log('save_ques ==== ',opt);
+    console.log('save_ques ==== ',ans);
 }
-
-
-
-
 
 html_ques = [
     {
