99a688bd8b954c4c88e625dfbbf5b1e06c86b851
rabiayousuf187 <rabiayousuf187@gmail.com>
2023-04-07 10:28:58 +0500

    new file in admin folder to perform HTML CRUD

---
+++ js/admin/html_crud.js
@@ -0,0 +1,114 @@
+import { html_ques } from "../ques/html_ques";
+console.log("html ques === push ",html_ques)
+
+let opt_array = [], opt = [];
+let ques,ans;
+let ques_obj = {};
+let ind = 0;
+let get_data = localStorage.getItem("html_ques");
+console.log("html ques === push ",get_data)
+if(get_data !== null){
+    get_data = JSON.parse(get_data);
+    console.log("html ques === push ",get_data)
+    get_data
+    html_ques.push(get_data);
+    console.log("html ques === push ",html_ques)
+}
+else{
+    console.log("NO html Quest in local storage");
+}
+
+// *******************************************
+$(document).ready(function() {
+    $('#example').DataTable({
+        scrollY: "40vh",
+        scrollCollapse: true,
+        scroller: true,
+        responsive: true
+    });
+
+    
+    
+} );
+
+document.getElementById('save_ques_btn').onclick = ()=>{ save_ques()};
+function save_ques(){
+    ques = document.getElementById('html_ques').value;
+    ans =  document.getElementById('ans').value;
+    opt_array = document.getElementById('options').querySelectorAll('[name = opt]');
+    opt_array = Object.values(opt_array)
+    console.log('opt_array  ==== ',typeof(opt_array));
+    console.log('opt_array  ==== ',opt_array);
+    console.log('opt_array  ==== ',opt_array[0]);
+    console.log('opt_array  ==== ',opt_array[0].value);
+
+    for( ; ind < opt_array.length ; ind++){
+        console.log('opt_array  ==== ',opt_array[ind].value);
+        opt.push(opt_array[ind].value);
+    }
+    console.log('save_ques ==== ', ques);
+    console.log('OPT final array ==== ',opt);
+    console.log('OPT final array ==== ',typeof(opt));
+    console.log('save_ques ==== ',ans);
+    ques_obj = {
+        "id" : ind + 1,
+        "ques" : "What do you understand by HTML?",
+        "opt" : [
+                "HTML describes the structure of a webpage",
+                "HTML is the standard markup language mainly used to create web pages",
+                "HTML consists of a set of elements that helps the browser how to view the content",
+                "All of the above"
+            ],
+        "ans" : "All of the above",
+    };
+}
+
+
+html_ques = [
+    {
+        "id" : "1",
+        "ques" : "What do you understand by HTML?",
+        "opt" : [
+                "HTML describes the structure of a webpage",
+                "HTML is the standard markup language mainly used to create web pages",
+                "HTML consists of a set of elements that helps the browser how to view the content",
+                "All of the above"
+            ],
+        "ans" : "All of the above",
+    },
+    {
+        "id" : "2",
+        "ques" : "What do you understand by HTML?",
+        "opt" : [
+            "HTML describes the structure of a webpage",
+            "HTML is the standard markup language mainly used to create web pages",
+                "HTML consists of a set of elements that helps the browser how to view the content",
+                "All of the above"
+            ],
+            "ans" : "All of the above",
+        },
+        {
+            "id" : "3",
+            "ques" : "What do you understand by HTML?",
+            "opt" : [
+                "HTML describes the structure of a webpage",
+                "HTML is the standard markup language mainly used to create web pages",
+                "HTML consists of a set of elements that helps the browser how to view the content",
+                "All of the above"
+            ],
+            "ans" : "All of the above",
+        },
+    {
+        "id" : "4",
+        "ques" : "What do you understand by HTML?",
+        "opt" : [
+                "HTML describes the structure of a webpage",
+                "HTML is the standard markup language mainly used to create web pages",
+                "HTML consists of a set of elements that helps the browser how to view the content",
+                "All of the above"
+            ],
+        "ans" : "All of the above",
+    }
+];
+
+export{ html_ques };
